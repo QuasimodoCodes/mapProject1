28c9a6bf4322290a7236b337155052883c6c4c55
Victor Hugo Figueroa <102181741+SpookyBugie@users.noreply.github.com>
2024-01-16 11:51:07 +0100

    added zoom to location and show all of norway

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,34 +1,83 @@
-import { MutableRefObject, useEffect, useRef } from "react";
-import { useGeographic } from "ol/proj";
-import { OSM } from "ol/source";
+import "./App.css";
+import "ol/ol.css";
+
 import { Map, View } from "ol";
+import { MutableRefObject, useEffect, useMemo, useRef } from "react";
 import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+
+useGeographic();
 
 function App() {
+  const view = useMemo(
+    () =>
+      new View({
+        center: [10.5, 59.7],
+        zoom: 11,
+      }),
+    []
+  );
+  const map = useMemo(
+    () =>
+      new Map({
+        layers: [
+          new TileLayer({ source: new OSM() }),
+          new VectorLayer({
+            source: new VectorSource({
+              url: "/kommuner.json",
+              format: new GeoJSON(),
+            }),
+          }),
+        ],
+        view: view,
+      }),
+    []
+  );
+
+  function handleZoomToUser(e: React.MouseEvent) {
+    e.preventDefault();
+
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { longitude, latitude } = pos.coords;
+      const center = [longitude, latitude];
+      map.getView().animate({ center, zoom: 14 });
+    });
+  }
+
+  function handleZoomToNorway(e: React.MouseEvent) {
+    e.preventDefault();
+
+    map.getView().animate({
+      center: [15, 65],
+      zoom: 5,
+    });
+  }
+
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
 
   useEffect(() => {
-    useGeographic();
-
-    const map = new Map({
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      view: new View({
-        center: [10, 59],
-        zoom: 8,
-      }),
-    });
-
     map.setTarget(mapRef.current);
   }, []);
 
   return (
-    <div className={"map"} ref={mapRef} style={{ height: "100vh", width: "100vw" }}>
-      I am a map
-    </div>
+    <>
+      <header>
+        <h1>Kristiania mapping application</h1>
+      </header>
+      <nav>
+        <a href={"#"} onClick={handleZoomToUser}>
+          Zoom to my location
+        </a>
+        <a href={"#"} onClick={handleZoomToNorway}>
+          Show all of Norway
+        </a>
+      </nav>
+      <div className={"map"} ref={mapRef}></div>
+    </>
   );
 }
 
